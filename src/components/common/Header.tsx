27422b8bfd7a4ed2e09526e9d06bdecee4cffe6f
Aditya Kumbhar <adityak4347@gmail.com>
2026-02-22 05:30:06 +0000

    Try fixing this error: `Console Error: In HTML, <a> cannot be a descenda

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -36,9 +36,7 @@ export function Header() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="mr-4 hidden md:flex">
-          <Link href="/" className="mr-6 flex items-center space-x-2">
-            <Logo />
-          </Link>
+          <Logo className="mr-6" />
         </div>
         <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
           <div className="md:hidden">
@@ -53,9 +51,7 @@ export function Header() {
                 <SheetHeader>
                    <SheetTitle className="sr-only">Main Menu</SheetTitle>
                 </SheetHeader>
-                <Link href="/" className="mb-6 flex items-center space-x-2">
-                    <Logo />
-                </Link>
+                <Logo className="mb-6" />
                 <nav className="flex flex-col space-y-4">
                   {navLinks.map((link) => (
                     <Link
